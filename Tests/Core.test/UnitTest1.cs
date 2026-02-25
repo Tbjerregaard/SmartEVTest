1effb4a1b0966cfa142c11fb0202ff71c10e29a6
bacon <dbech22@student.aau.dk>
2026-02-25 15:45:39 +0100

    Init

---
+++ Tests/Core.test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Core;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Tests/Simulation.test/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Simulation;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
