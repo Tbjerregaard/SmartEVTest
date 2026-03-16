997b6e636b414a7ac9c184ce3f6022cd098f5864
MertzAndreas <Andreasmertz11@gmail.com>
2026-03-16 11:42:55 +0100

    Interpolate position

---
+++ Tests/Core.test/Routing/JourneyTests.cs
@@ -0,0 +1,62 @@
+namespace Core.test.Routing;
+
+using Core.Routing;
+using Core.Shared;
+
+public class JourneyTests
+{
+    [Fact]
+    public void JourneyInterpolationEnd()
+    {
+        var waypoints = new List<Position>
+        {
+            new(1, 1),
+            new(1, 2),
+        };
+        var journey = new Journey(departure: 0, duration: 2, new Paths(waypoints));
+        var expectedEndpoint = journey.CurrentPosition(2);
+        Assert.Equal(waypoints[1].Latitude, expectedEndpoint.Latitude);
+    }
+
+    [Fact]
+    public void JourneyInterpolationStart()
+    {
+        var waypoints = new List<Position>
+        {
+            new(1, 1),
+            new(1, 2),
+        };
+        var journey = new Journey(departure: 0, duration: 2, new Paths(waypoints));
+        var expectedEndpoint = journey.CurrentPosition(0);
+        Assert.Equal(waypoints[0].Latitude, expectedEndpoint.Latitude);
+    }
+
+    [Fact]
+    public void JourneyInterpolationBetween()
+    {
+        var waypoints = new List<Position>
+        {
+            new(1, 1),
+            new(1, 2),
+        };
+        var journey = new Journey(departure: 0, duration: 2, new Paths(waypoints));
+        var expectedEndpoint = journey.CurrentPosition(1);
+        Assert.Equal(1.5, expectedEndpoint.Latitude);
+    }
+
+    [Fact]
+    public void JourneyInterpolationBetweenMultipleLineSegments()
+    {
+        var waypoints = new List<Position>
+        {
+            new(1, 1),
+            new(1, 2),
+            new(1, 3),
+            new(1, 4),
+            new(1, 5),
+        };
+        var journey = new Journey(departure: 0, duration: 4, new Paths(waypoints));
+        var expectedEndpoint = journey.CurrentPosition(3);
+        Assert.Equal(waypoints[3].Latitude, expectedEndpoint.Latitude);
+    }
+}
